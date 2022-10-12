3cddf4a21b2a5efc6812bd6deefdc6ae74694aae
Tahsan <tahsanknmas14@gmail.com>
2022-10-12 21:02:26 +0600

    added require auth

--- src/pages/Appointment/Service.js
+++ src/pages/Appointment/Service.js
@@ -11,7 +11,7 @@ const Service = ({ service, setTreathment }) => {
                 }</p>
                 <p> {slots.length} {slots.length > 1 ? 'spaces' : 'space'} available</p>
                 <div className="card-actions justify-center">
-                    <label for="booking-modal"
+                    <label htmlFor="booking-modal"
                         disabled={slots.length === 0}
                         onClick={() => setTreathment(service)}
                         className="btn btn-sm btn-secondary text-white uppercase bg-gradient-to-r from-secondary to-primary">Book Appointment</label>
